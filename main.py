19ac9a5ae1786dec2f2b578167312524d6763f21
mwear1021 <mwear1021@gmail.com>
2026-03-17 20:18:40 -0400

    implemented loop structure

--- main.py
+++ main.py
@@ -10,36 +10,41 @@ import os
 import requests
 from dotenv import load_dotenv
 load_dotenv()
-
-#import API key from .env
+# import API key from .env
 WEATHER_KEY = os.getenv('WEATHER_API_KEY')
 
-get_city = input("Enter city name: ")
+# main loop
+while True:
 
-#convert city into coordinates
-geo_url = f"http://api.openweathermap.org/geo/1.0/direct?q={get_city},&limit=1&appid={WEATHER_KEY}"
-geo_response = requests.get(geo_url)
-geo_data = geo_response.json()
-# print(geo_data)
+    get_city = input("Enter city name or type 'exit' to quit: ")
+    if get_city.lower() == 'exit':
+        exit()
+    else:
+        #convert city into coordinates
+        geo_url = f"http://api.openweathermap.org/geo/1.0/direct?q={get_city},&limit=1&appid={WEATHER_KEY}"
+        geo_response = requests.get(geo_url)
+        geo_data = geo_response.json()
+        # print(geo_data)
 
-#safety check
-if geo_data:
-    lat = geo_data[0]['lat']
-    lon = geo_data[0]['lon']
-    print(f"Coordinates found for {get_city}: Latitude: {lat}, Longitude: {lon}")
-else:
-    print("City not found")
-    exit()
-# weather url
-current_weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={WEATHER_KEY}"
+        #safety check
+        if geo_data:
+            lat = geo_data[0]['lat']
+            lon = geo_data[0]['lon']
+            print(f"Coordinates found for {get_city.title()}: Latitude: {lat}, Longitude: {lon}")
+        else:
+            print("City not found. Please try again")
+            continue
+        # weather url
+        current_weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={WEATHER_KEY}"
 
-#get API data
-weather_response = requests.get(current_weather_url)
-weather_data = weather_response.json()
+        #get API data
+        weather_response = requests.get(current_weather_url)
+        weather_data = weather_response.json()
 
-temp = weather_data['main']['temp'] # main is a json dictionary
-feels_like = weather_data['main']['feels_like']
-conditions = weather_data['weather'][0]['main'] # weather is a list so must index it with 0
+        temp = weather_data['main']['temp'] # main is a json dictionary
+        feels_like = weather_data['main']['feels_like']
+        conditions = weather_data['weather'][0]['main'] # weather is a list so must index it with 0
 
-print(f"Weather conditions for {get_city}:\nCurrent temperature: {temp}°F\nFeels like: {feels_like}°F\nConditions: {conditions}")
+        print(f"Weather conditions for {get_city}:\nCurrent temperature: {temp}°F\nFeels like: {feels_like}°F\nConditions: {conditions}")
+        continue
 
